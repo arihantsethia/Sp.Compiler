7ee4125250e4a1f52e8d9e8aff2be377aaf21313
arihantsethia <arihantsethia07@gmail.com>
2014-01-22 19:06:58 +0530

    Intital Setup

---
+++ Assignment 1/compiler_assn/main.c
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+#include "FRONT.h"
+
+using namespace std ;
+
+
+int main()
+{
+
+	return 0 ;
+}
---
+++ Assignment 1/compiler_assn/main.cpp
@@ -0,0 +1,11 @@
+#include "FRONT.h"
+#include "var.h"
+#include<iostream>
+
+using namespace std ;
+int main()
+{
+	FRONT compile ;
+	compile.statements() ;
+	return 0 ;
+}
