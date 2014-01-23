94064dec60dd1f962ecdbbfa608487fdd6d9ea7f
arihantsethia <arihantsethia07@gmail.com>
2014-01-24 02:29:06 +0530

    partial x86 code generation done

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -1,17 +1,18 @@
-#include "code_gen.h"
 #include "lex.h"
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
+#include "code_gen.h"
 
-using namespace std;
-
-int main ()
-{
+int main () {
+    printf("global _start\nsection .text\n_start:\n");
     statement ();
-    if(!match(EOI)){
-       fprintf( stderr, "%sLine %d %s\'EOI\'%s expected\n",KBLU,yylineno,KRED,KNRM);
-       exit(1);
+    if(!match(EOI)) {
+        fprintf( stderr, "%sLine %d %s\'EOI\'%s expected\n",KBLU,yylineno,KRED,KNRM);
+        exit(1);
     }
+    printf(" mov rax,60\nmov rdi,0\nsyscall\n");
     return 0;
 }
+
+
+/*nasm -f elf64 -o hello64.o hello64.nasm */
+/*ld -o hello64 hello64.o*/
+/*./hello64*/
