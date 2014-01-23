004fc45ccd6100334059c8ad933f5d5ce18a53ac
Arihant Sethia <arihantsethia07@gmail.cim>
2014-01-23 20:03:44 +0530

    Colored Error Message Added

--- Assignment 1/lex.h
+++ Assignment 1/lex.h
@@ -28,3 +28,13 @@ extern int yylineno;
 int match(int);
 void advance(void);
 bool is_number(char * s , int len);
+
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[1;31m"
+#define KGRN  "\x1B[1;32m"
+#define KYEL  "\x1B[1;33m"
+#define KBLU  "\x1B[1;34m"
+#define KMAG  "\x1B[1;35m"
+#define KCYN  "\x1B[1;36m"
+#define KWHT  "\x1B[1;37m"
