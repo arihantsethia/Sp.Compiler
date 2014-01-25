38cb9eb3131c7d85d8c11081b7b1db8181d253ae
Arihant Sethia <arihantsethia07@gmail.cim>
2014-01-25 18:26:33 +0530

    Variable Handling Done

--- Assignment 1/name.cpp
+++ Assignment 1/name.cpp
@@ -2,21 +2,24 @@
 #include <cstdlib>
 #include "lex.h"
 
-char  *Names[] = { "rax", "rbx", "rcx", "rdx" }; //, "esi", "edi"
+char  *Names[] = { "%rax", "%rbx", "%rcx", "%rdx" }; //, "esi", "edi"
 char  **Namep  = Names;
 
-char  *newname() {
-    if( Namep >= &Names[ sizeof(Names)/sizeof(*Names) ] ) {
-        fprintf( stderr, "%d: Expression too complex\n", yylineno );
-        exit( 1 );
-    }
-    return( *Namep++ );
+char  *newname()
+{
+	if( Namep >= &Names[ sizeof(Names)/sizeof(*Names) ] )
+	{
+		fprintf( stderr, "%d: Expression too complex\n", yylineno );
+		exit( 1 );
+	}
+	return( *Namep++ );
 }
 
-void freename(char    *s) {
-    if( Namep > Names )
-        *--Namep = s;
-    else
-        fprintf(stderr, "%d: (Internal error) Name stack underflow\n",
-                yylineno );
+void freename(char    *s)
+{
+	if( Namep > Names )
+		*--Namep = s;
+	else
+		fprintf(stderr, "%d: (Internal error) Name stack underflow\n",
+				yylineno );
 }
