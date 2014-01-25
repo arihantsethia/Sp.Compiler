e07c9b7a87ce5a708d8066aceae537c903270136
arihantsethia <arihantsethia07@gmail.com>
2014-01-25 18:26:33 +0530

    Variable Handling Done

--- Assignment 1/code_gen.h
+++ Assignment 1/code_gen.h
@@ -1,6 +1,5 @@
 #ifndef CODE_GEN_H_INCLUDED
 #define CODE_GEN_H_INCLUDED
-#include "Identifier.h"
 
 void statement();
 char    *factor     ( void );
@@ -14,5 +13,6 @@ extern void freename( char *name );
 extern FILE *fp;
 static vector< map<string, int> > ids(100);
 static int scope = 0;
-static int stackpos = 0;
+static int stackpos = 4;
+
 #endif // CODE_GEN_H_INCLUDED
