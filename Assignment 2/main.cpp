86093265e257438f94ef5183866587f7097b6af4
arihantsethia <arihantsethia07@gmail.com>
2014-02-09 02:33:22 +0530

    Lexical Analyzer updated to handle a generic c-code. Support for 'octa' & 'hex' numbers + MACROS yet to be added.

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
-
+#include "header.h"
+#include "keywords.h"
+#include "lex.h"
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    string s;
+   int d = 1;
+   while(d){
+        d =yylex();
+        s.assign(yytext, yytext + yyleng);
+        if(d>=1 && d<=33)
+            cout<<"< "<<s<<" , keyword >\n";
+        else if(d>=40 && d<=71)
+            cout<<"< "<<s<<" , operator >\n";
+        else if(d>=80 && d<=94)
+            cout<<"< "<<s<<" , symbol >\n";
+        else if(d>=95 && d<=98)
+            cout<<"< "<<s<<" , constant >\n";
+        else if(d==99)
+            cout<<"< "<<s<<" , identifier >\n";
+        else if(d==100)
+            cout<<"< "<<s<<" , string >\n";
+        else if(d==101)
+            cout<<"< "<<s<<" , character >\n";
+    }
     return 0;
 }
---
+++ Assignment 2/syntax_analyzer.h
@@ -0,0 +1,10 @@
+#ifndef SYNTAX_ANALYZER_H_INCLUDED
+#define SYNTAX_ANALYZER_H_INCLUDED
+
+//static int Lookahead = -1; /* Lookahead token  */
+/*
+extern char *yytext;
+extern int yyleng;
+extern int yylineno;
+*/
+#endif // SYNTAX_ANALYZER_H_INCLUDED
